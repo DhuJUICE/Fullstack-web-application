4a9769672abe3474a8cfe5556591e4c9a73b4008
Ayanda2002 <133391807+Ayanda2002@users.noreply.github.com>
2024-08-13 08:23:24 +0200

    Placed PostgreSQL on AWS cloud.

--- ProjectBackend/ProjectBackend/settings.py
+++ ProjectBackend/ProjectBackend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ProjectBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Share2TeachDB',
+        'USER': 'postgres',
+        'PASSWORD': 'giovanI200',
+        'HOST': 'share2teach-db.c70w2o822mxq.af-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
