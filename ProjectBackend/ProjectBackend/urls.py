3df05fae68304f78a61365c44a6dfb1fdcd25de8
TS29TR21 <169053192+TS29TR21@users.noreply.github.com>
2024-08-15 13:48:19 +0200

    added url to s3

--- ProjectBackend/ProjectBackend/urls.py
+++ ProjectBackend/ProjectBackend/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.testpage, name="testpage"),
+    path('test-s3/', test_s3_connection, name='test_s3_connection'),
 ]
