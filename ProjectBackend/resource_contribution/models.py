dd80b03e86e7fdb7dcca23925cc69008c47feaec
DhuJUICE <junierherandien84@gmail.com>
2024-08-22 22:23:42 +0200

    added resource-metadata model

--- ProjectBackend/resource_contribution/models.py
+++ ProjectBackend/resource_contribution/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class ResourceMetadata(models.Model):
+
+    #resource-metadata
+    File_Path = ""
+    File_Type = ""
+    
+    Resource_Name = ""
+    Subject = ""
+    Grade = ""
+    Keywords = ""
+    Date_Contributed = ""
+    
+    Resource_Rating = ""
+    
+    Approval_Status = ""
+    Moderation_Comment = ""
+    Moderation_Date = ""
