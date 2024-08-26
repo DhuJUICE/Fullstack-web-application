e2a714c4431ff3c2f5855600cf8336ac88a93a93
Juniër <102233050+DhuJUICE@users.noreply.github.com>
2024-08-27 00:06:00 +0200

    registered the Resource_Report model

--- ProjectBackend/resource_report/models.py
+++ ProjectBackend/resource_report/models.py
@@ -6,3 +6,4 @@ class RESOURCE_REPORT(models.Model):
     #Resource Report
     reportResource = models.ForeignKey(RESOURCE_METADATA, on_delete=models.CASCADE)
     reportComplaint = models.CharField(max_length = 100)
+    reportDatetime = models.DateTimeField(auto_now_add=True)
