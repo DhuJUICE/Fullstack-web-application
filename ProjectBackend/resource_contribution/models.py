2ec3a46a5b5b9989be73685c780d813c1274a145
Juniër <102233050+DhuJUICE@users.noreply.github.com>
2024-08-26 23:54:43 +0200

    added all the initial fields for resourceMetadata table

--- ProjectBackend/resource_contribution/models.py
+++ ProjectBackend/resource_contribution/models.py
@@ -4,19 +4,29 @@ from django.db import models
 class ResourceMetadata(models.Model):
 
     #Resource Metadata
-    File_Path = ""
-    File_Type = ""
-    
-    Contributor = ""
-    
-    Resource_Name = ""
-    Subject = ""
-    Grade = ""
-    Keywords = ""
-    Date_Contributed = ""
-    
-    Resource_Rating = ""
-    
-    Approval_Status = ""
-    Moderation_Comment = ""
-    Moderation_Date = ""
+    #identifier from aws s3
+    #file_Path = models.FileField(upload_to='aws')
+
+    #what type of file is being stored as the resource
+    file_type = models.CharField(max_length = 100)
+
+    #get the id of the person uploading the resource
+    contributor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    #details about the specific resource
+    resource_name = models.CharField(max_length = 100)
+    subject = models.CharField(max_length = 100)
+    grade = models.CharField(max_length = 100)
+
+    #keywords contains a list of keywords to find the resource with(need an array later on)
+    keywords = models.CharField(max_length = 100)
+
+    date_contributed = models.DateTimeField(auto_now_add=True)
+
+    #rating of the resource
+    resource_rating = models.IntegerField()
+
+    #moderation details
+    approval_status = models.CharField(max_length = 100)
+    moderation_comment = models.CharField(max_length = 100)
+    moderation_date = models.DateTimeField()
