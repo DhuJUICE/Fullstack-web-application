2441220dc42fae809a6f7beb8545db90f4821c22
Ayanda2002 <133391807+Ayanda2002@users.noreply.github.com>
2024-08-27 20:36:30 +0200

    Pagination Settings
    This setting uses the PageNumberPagination class and limits API responses to 5 items per page.

--- ProjectBackend/ProjectBackend/settings.py
+++ ProjectBackend/ProjectBackend/settings.py
@@ -125,6 +125,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,  # Number of items per page
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
