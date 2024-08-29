7b0d6be2da47ed30800f875b0647fc4d438778c2
DhuJUICE <junierherandien84@gmail.com>
2024-08-29 12:57:45 +0200

    added all serializers details for faq

--- ProjectBackend/faq/urls.py
+++ ProjectBackend/faq/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import FaqListCreate, FaqDetail
 
 urlpatterns = [
-    path('books/', FaqListCreate.as_view(), name='faq-list-create'),
+    path('faq/', FaqListCreate.as_view(), name='faq-list-create'),
     #path('books/<int:pk>/', FaqDetail.as_view(), name='faq-detail'),
 ]
