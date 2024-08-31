96be94fbb31505023529fb14b399f2f876f78507
JUNIER HERANDIEN <38983494@adt.nwu.ac.za>
2024-08-31 09:12:01 +0200

    rolled back a failed attempt

---
+++ ProjectBackend/rest_API/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import FAQ
+
+class FaqSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FAQ
+        
+        fields = '__all__'  # or specify fields explicitly
