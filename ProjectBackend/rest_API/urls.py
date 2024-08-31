0deab7f39ad2e2ece7062a80686882a788c3c437
JUNIER HERANDIEN <38983494@adt.nwu.ac.za>
2024-08-31 21:26:00 +0200

    update

--- ProjectBackend/rest_API/urls.py
+++ ProjectBackend/rest_API/urls.py
@@ -3,22 +3,38 @@ from django.contrib import admin
 from django.urls import path
 from .views import serializeFaq, serializeResource, serializeReport, serializeUser
 from .views import deserializeFaq, deserializeResource, deserializeReport, deserializeUser
+from django.test import TestCase
+
 
 urlpatterns = [
-    # Serialize routes
+	#serialize routes
+	#to see all objects from a table
     path('api/faq/serial/', serializeFaq.as_view(), name='faq-list-create'),
-    path('api/resource/serial/', serializeResource.as_view(), name='doc-list-create'),
-    path('api/report/serial/', serializeReport.as_view(), name='report-list-create'),
-    path('api/user/serial/', serializeUser.as_view(), name='user-list-create'),
+	
+	#serialize resource endpoint
+	path('api/resource/serial/', serializeResource.as_view(), name='doc-list-create'),
+	
+	#serialize report endpoint
+	path('api/report/serial/', serializeReport.as_view(), name='report-list-create'),
+	
+	#serialize User endpoint
+	path('api/user/serial/', serializeUser.as_view(), name='user-list-create'),
 
-    # Deserialize routes
-    path('api/faq/deserial/', deserializeFaq.as_view(), name='faq-create'),
-    path('api/faq/deserial/<int:pk>/', deserializeFaq.as_view(), name='faq-detail'),
-    path('api/resource/deserial/', deserializeResource.as_view(), name='doc-create'),
-    path('api/resource/deserial/<int:pk>/', deserializeResource.as_view(), name='doc-detail'),
-    path('api/report/deserial/', deserializeReport.as_view(), name='report-create'),
-    path('api/report/deserial/<int:pk>/', deserializeReport.as_view(), name='report-detail'),
-    path('api/user/deserial/', deserializeUser.as_view(), name='user-create'),
-    path('api/user/deserial/<int:pk>/', deserializeUser.as_view(), name='user-detail'),
+
+	#deserialize routes
+	#deserialize faq endpoint
+    path('api/faq/deserial', deserializeFaq.as_view(), name='faq'),
+	path('api/faq/deserial/<int:pk>', deserializeFaq.as_view(), name='faq'),
+	
+	#deserialize resource endpoint
+	path('api/resource/deserial/', deserializeResource.as_view(), name='doc'),
+	path('api/resource/deserial/<int:pk>', deserializeFaq.as_view(), name='faq'),
+	
+	#deserialize report endpoint
+	path('api/report/deserial/', deserializeReport.as_view(), name='report'),
+	path('api/report/deserial/<int:pk>', deserializeFaq.as_view(), name='faq'),
+	
+	#deserialize User endpoint
+	path('api/user/deserial/', deserializeUser.as_view(), name='user'),
+	path('api/user/deserial/<int:pk>', deserializeFaq.as_view(), name='faq'),
 ]
-
