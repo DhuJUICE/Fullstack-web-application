b6eddd7a099df4f325850825f329ef6bc5e94146
JUNIER HERANDIEN <38983494@adt.nwu.ac.za>
2024-08-31 21:19:42 +0200

    update

--- ProjectBackend/ProjectBackend/settings.py
+++ ProjectBackend/ProjectBackend/settings.py
@@ -162,4 +162,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+SECURE_SSL_REDIRECT = False
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
