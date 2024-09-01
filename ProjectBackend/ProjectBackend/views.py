f02af70c703b14bd3e7b183f16ab1a30fa803b95
Juniër <102233050+DhuJUICE@users.noreply.github.com>
2024-09-01 18:04:26 +0200

    Revert "Merge remote-tracking branch 'origin/branch-loggingTest' into branch-modelRefinement"
    This reverts commit a639ae80473a5208711a6a5e969588447fcb7197, reversing
    changes made to dbccbcd6e6ad059759a5a81b526e95436602702b.

--- ProjectBackend/ProjectBackend/views.py
+++ ProjectBackend/ProjectBackend/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import requests
 
 def testpage(request):
-	return render(request, 'APIdelete.html')
+	return render(request, 'testpage.html')
 
 import boto3
 from django.http import JsonResponse
