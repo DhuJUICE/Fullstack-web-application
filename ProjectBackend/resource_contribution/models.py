4e8332d9c9766c0398a4622e46215feafabe0165
DhuJUICE <junierherandien84@gmail.com>
2024-09-07 07:54:21 +0200

    update

--- ProjectBackend/resource_contribution/models.py
+++ ProjectBackend/resource_contribution/models.py
@@ -5,7 +5,7 @@ class RESOURCE_METADATA(models.Model):
 
     #Resource Metadata
     #identifier from aws s3
-    file_path = models.FileField(upload_to='resources/')  # Automatically handles file storage
+    file_path = models.FileField(upload_to='resources/')  # Files will be stored under 'resources/' in S3
 
     #what type of file is being stored as the resource
     file_type = models.CharField(max_length = 100)
