d9be770b7a5356710b92da634d2a856eeb92a2d1
DhuJUICE <junierherandien84@gmail.com>
2024-09-08 01:28:57 +0200

    updated the user role in UserProfile

--- ProjectBackend/user_management/models.py
+++ ProjectBackend/user_management/models.py
@@ -5,6 +5,15 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=100, blank=True, null=True)
+
+    #options for users as roles
+    USER_ROLES_CHOICES = [
+    ('openUser', 'Open Access'),
+    ('adminUser', 'Admin'),
+    ('educatorUser', 'Educator'),
+    ('moderatorUser', 'Moderator'),
+]
+    #moderation details
+    role = models.CharField(max_length=20, choices=USER_ROLES_CHOICES, default='openUser')
     image = models.CharField(max_length=100, blank=True, null=True)
     verificationCode = models.CharField(max_length=100, blank=True, null=True)
