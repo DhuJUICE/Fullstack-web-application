0f2b02f5b8a16fa00384595033834aa4177ba95b
DhuJUICE <junierherandien84@gmail.com>
2024-09-08 02:36:37 +0200

    completed FAQ functionality for viewing faqs

--- ProjectBackend/faq/urls.py
+++ ProjectBackend/faq/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
+from .views import displayFaqs
 
 urlpatterns = [
-	#example path
-    #path('books/<int:pk>/', FaqDetail.as_view(), name='faq-detail'),
+	#url to display all faqs from database
+    path('faqs', displayFaqs, name='faqs-display'),
 ]
--- ProjectBackend/faq/views.py
+++ ProjectBackend/faq/views.py
@@ -1,1 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+#display all the FAQS from database
+def displayFaqs(request):
+	return redirect("/api/faq/deserial/paginated")
