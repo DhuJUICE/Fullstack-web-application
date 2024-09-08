093a397e967001ff96f56dde80dac236f63445ba
DhuJUICE <junierherandien84@gmail.com>
2024-09-08 03:36:23 +0200

    set up the url and views function of resource Searching functionality

--- ProjectBackend/document_search/urls.py
+++ ProjectBackend/document_search/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('pageName', views.functionName, name='functionName'),
+    path('resourceSearch', views.resourceSearch, name='resourceSearch'),
 ]
--- ProjectBackend/document_search/views.py
+++ ProjectBackend/document_search/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+#search document resources based on subjects or keywords, or search in subject based on keywords
+def resourceSearch(request):
+	#get the resources from database based on subject
+	
+	#get the resources from database based on keywords
+
+	#get the resources from database based on subject and keywords
+
+	#return the page with the search results
+	return None
