9d565d23a8106b3b441f0419149c37e774e4f0fc
DhuJUICE <junierherandien84@gmail.com>
2024-09-08 03:04:35 +0200

    added comment guides for rating function

--- ProjectBackend/resource_review/views.py
+++ ProjectBackend/resource_review/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+#get the resources from database and rate them, then save them back in the database
 def resourceRating(request):
+	#get resources from database with intial empty rating
+
+	#rate the resource
+
+	#save resource with updated rating
+
+	#return the rating page with updated rating
 	return None
