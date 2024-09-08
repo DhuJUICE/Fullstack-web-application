6f169f36c9681d9f4a528e58f177435fd1d9996d
DhuJUICE <junierherandien84@gmail.com>
2024-09-08 04:20:54 +0200

    set up all functionalities
    Now we can just go from this template of what should be done in each views.py file for each functionality

--- ProjectBackend/resource_contribution/views.py
+++ ProjectBackend/resource_contribution/views.py
@@ -6,28 +6,31 @@ def resourceUploading(request):
 	#get all the details of the resource(including file type) alongside the document(s) to upload
 
 	#get the files ready to be converted to pdf
+	resourcePdfConversion() #pass the files to convert
 	return None
 
 #function to handle pdf conversion
-def resourcePdfConversion(request):
+def resourcePdfConversion():
 	#get the resource document(s) to convert, must handle a few file types
 
 	#convert the document(s) to pdf
 
 	#get the files ready to be prepended with watermark/licence
+	resourceLicencePrepending() #pass the files to prepend licence to
 	return None
 
 #function to handle watermark/licence prepending
-def resourceLicencePrepending(request):
+def resourceLicencePrepending():
 	#get the pdf versions of the resources to be uploaded to file storage
 
 	#prepend the license/watermark to the pdf document(s) 
 
 	#get the files ready to be uploaded to the file storage system
+	resourceFileStorage() #save the files to the File Storage System
 	return None
 
 #function to handle file system storage
-def resourceFileStorage(request):
+def resourceFileStorage():
 	#get the pdf resource with prepended watermark/license
 
 	#upload/save the document/resources to the file storage system
