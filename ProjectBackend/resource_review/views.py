78862c4295e3ac5f29c7057a5a310bbdedef6157
TS29TR21 <169053192+TS29TR21@users.noreply.github.com>
2024-09-13 11:17:00 +0200

    resource moderation views

--- ProjectBackend/resource_review/views.py
+++ ProjectBackend/resource_review/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from resource_contribution.models import RESOURCE_METADATA
 # Create your views here.
 #get the resources from database and rate them, then save them back in the database
 def resourceRating(request):
@@ -14,11 +14,22 @@ def resourceRating(request):
 
 #get the resources from database and moderate them, then save them back in the database
 def resourceModeration(request):
-	#get resources from database with intial empty moderation comment and pending approval
+
+	resourceId = '1'
+	approval_status = 'approved'
+	moderation_comment = 'A cake/cupcake would work rn'
+	moderation_date = timezone.now()
+
+	#get resources from database with initial empty moderation comment and pending approval
+	resource = RESOURCE_METADATA.objects.get(pk=kwargs['pk'])
 
 	#moderate the resource
+	resource.approval_status = approval_status
+	resource.moderation_comment = moderation_comment
+	moderation_date = timezone.now()
 
 	#save resource with updated moderation details
+	resource.save()
 
 	#return the moderation page with updated moderation details
 	return None
