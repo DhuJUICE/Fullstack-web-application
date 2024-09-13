53112046ce0fa7503a89a2cab967f2cc25c117aa
DhuJUICE <junierherandien84@gmail.com>
2024-09-13 12:05:22 +0200

    set up api usage for CRUD

--- ProjectBackend/resource_report/views.py
+++ ProjectBackend/resource_report/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import RESOURCE_REPORT
+import requests
 
 # Create your views here.
 #resport certain resource
 def resourceReport(request):
+	"""
 	#get the currently logged in user - must add resourceUser to the report model
-	#user = request.user
+	user = request.user
 
 	#get the resource compaint from the frontend
 	complaint = request.POST.get('resourceComplaint')	
@@ -13,6 +15,21 @@ def resourceReport(request):
 
 	#create the resource report object to be recorded
 	report = RESOURCE_REPORT.objects.create(reportResource=resource, reportComplaint=complaint)
+	"""
 
+	api_url = 'http://127.0.0.1:8000/api/faq/deserial'  # Replace with your actual API endpoint
+	data = {
+        'question': 'newQuestion',
+        'answer': 'newAnswer'
+    }
+	headers = {'Content-Type': 'application/json'}
+	response = requests.post(api_url, json=data, headers=headers)
+
+	return redirect("api/faq/deserial")
+
+	    
+	    
+    
+    
 	#save resource report to database
 	return None
